29656ab34511d7365939053b49074155b126215e
marcelo.silva <masilvasql@gmail.com>
2021-07-05 13:14:14 -0300

    Redimensionamento de imagens

--- pythonCodes/step2/OCR7.py
+++ pythonCodes/step2/OCR7.py
@@ -4,16 +4,16 @@ Redimensionamento de imagens
 '''
 import cv2
 
-img = cv2.imread('../../Util/Imagens/img-process.jpg')
+img = cv2.imread('../../Util/icon.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 invert = 255 - gray
 
-maior = cv2.resize(gray, None, fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
+maior = cv2.resize(gray, None, fx=3.5, fy=3.5, interpolation=cv2.INTER_CUBIC)
 
 menor = cv2.resize(gray, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
-cv2.imshow("image", menor)
+cv2.imshow("image", maior)
 cv2.waitKey(0)
 
---
+++ pythonCodes/step2/OCR8.py
@@ -0,0 +1,35 @@
+'''
+Operações morfológicas
+Erosão
+Dilatação
+Abertura
+Fechamento
+'''
+
+import cv2
+import numpy as np
+
+img = cv2.imread('../../Util/icon.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+erosao = cv2.erode(gray, np.ones((3,3),np.uint8))
+
+dilatacao = cv2.dilate(gray, np.ones((3,3), np.uint8))
+
+erosao2 = cv2.erode(gray, np.ones((5,5),np.uint8))
+
+'Abertura é a erosão + dilatação'
+abertura = cv2.dilate(erosao2, np.ones((5,5), np.uint8))
+
+
+'Fechamento - Dialtação + erosão'
+img2 = cv2.imread('../../Util/Imagens/texto-opencv2.jpg')
+gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+
+dilatacao2 = cv2.dilate(gray, np.ones((5,5)))
+Fechamento = cv2.erode(dilatacao2, np.ones((5,5)))
+
+
+cv2.imshow("img", erosao)
+cv2.waitKey(0)
